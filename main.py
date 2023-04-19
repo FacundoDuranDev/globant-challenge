5008ae026bae37843f8d2a0a927199c81857d811
facundo duran <facu.javi.du@gmail.com>
2023-04-19 14:56:13 -0300

    postgres sql connection tests

--- main.py
+++ main.py
@@ -1,30 +1,34 @@
-from pyspark.sql import SparkSession
-from pyspark import SparkContext
+from pyspark.sql import SparkSession , SQLContext
+from pyspark import SparkContext, SparkConf
 from FileSchemas import FileSchemas
 from flask import Flask, request
 
 
 app = Flask(__name__)
+spark = SparkSession.builder \
+    .appName("PostgreSQL example") \
+    .config("spark.driver.extraClassPath", "/home/facundo/jars/postgresql-42.6.0.jar") \
+    .getOrCreate()
 
+properties = {
+"user": "admin",
+"password": "Passw0rd",
+"driver": "org.postgresql.Driver"
+} 
 @app.route("/example",methods=["POST"])
 def example_post():
     filename = request.form['filename']
-    schema = request.form["schema"]
+    schema_type  = request.form["schema"]
 
-    if schema == "departaments":
-        schema = FileSchemas().dept_schema
-    if schema == "jobs":
-        schema = FileSchemas().job_schema
-    if schema == "hired":
-        schema = FileSchemas().hired_schema
-    spark = SparkContext("local")
-
-    dataframe = spark.read.csv(filename).schema(schema)
-    dataframe.show()
-    return dataframe.count()
-
-
-
+    if schema_type == "departments":
+        schema = FileSchemas().dept_schema()
+    if schema_type == "jobs":
+        schema = FileSchemas().job_schema()
+    if schema_type == "employees":
+        schema = FileSchemas().hired_schema()
+    dataframe = spark.read.jdbc("jdbc:postgresql://localhost:5433/postgres", "employees", properties=properties)
     
+    return f"{dataframe.count()}"
+
 if __name__ == '__main__':
     app.run(port=8000)
